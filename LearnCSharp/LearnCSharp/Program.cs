3f5e5b7c21ce44b612cdc30e7ffa1c964d0ec377
Daniil <notdedinside@icloud.com>
2022-02-12 11:19:17 +0300

    PRACTICE-TL Сделал домашнее задание

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using LearnCSharp;
+
+class Program
+{
+    public static void Main( string[] args )
+    {
+        Point leftTopPoint = new Point
+        {
+            X = 10,
+            Y = 10
+        };
+        Point rightBottomPoint = new Point
+        {
+            X = 15,
+            Y = 2
+        };
+        try
+        {
+            Rectangle rectangle = new Rectangle( leftTopPoint, rightBottomPoint );
+            int square = rectangle.GetSquare();
+            Console.WriteLine( square );
+            Console.WriteLine( $"{rectangle.LeftTopPoint.ToString()} {rectangle.RightBottomPoint.ToString()}" );
+        }
+        catch ( ArgumentException ex )
+        {
+            Console.WriteLine( ex.Message );
+        }
+    }
+}
---
+++ LearnCSharp/LearnCSharp/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿namespace LearnCSharp
+{
+    public class Rectangle
+    {
+        public Point LeftTopPoint { get; private set; }
+        public Point RightBottomPoint { get; private set; }
+
+        public Rectangle( Point leftTopPoint, Point rightBottomPoint )
+        {
+            if ( leftTopPoint.Y < rightBottomPoint.Y )
+            {
+                throw new ArgumentException( "Right bottom point is higher than left top point" );
+            }
+            if (leftTopPoint.X > rightBottomPoint.X )
+            {
+                throw new ArgumentException( "Left top point is righter than right bottom point" );
+            }
+
+            LeftTopPoint = leftTopPoint;
+            RightBottomPoint = rightBottomPoint;
+        }
+
+        public int GetSquare()
+        {
+            int height = LeftTopPoint.Y - RightBottomPoint.Y;
+            int width = RightBottomPoint.X - LeftTopPoint.X;
+            return height * width;
+        }
+    }
+}
